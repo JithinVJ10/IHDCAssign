f4024cd861b95943fe500df5b5420acb1629c6c8
jithin <jithinjolly15@gmail.com>
2024-02-11 14:33:43 +0530

    mobile view fix

--- Frontend/src/Components/NavBar.jsx
+++ Frontend/src/Components/NavBar.jsx
@@ -52,7 +52,7 @@ const NavBar = () => {
                 <ul
                     className={
                         nav
-                            ? 'fixed md:hidden left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500'
+                            ? 'fixed md:hidden left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500 z-10'
                             : 'ease-in-out w-[60%] duration-500 fixed top-0 bottom-0 left-[-100%]'
                     }
                 >
@@ -60,7 +60,7 @@ const NavBar = () => {
                     {navItems.map(item => (
                         <li
                             key={item.id}
-                            className='p-4 border-b rounded-xl hover:bg-[#00df9a] duration-300 hover:text-black cursor-pointer border-gray-600'
+                            className='p-4 text-white border-b rounded-xl hover:bg-[#00df9a] duration-300 hover:text-black cursor-pointer border-gray-600'
                         >
                             {item.text}
                         </li>
