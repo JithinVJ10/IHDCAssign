fed7157b41283985c9dbdb028aeb69be62a06142
jithin <jithinjolly15@gmail.com>
2024-02-11 11:04:28 +0530

    carousel fix

--- Frontend/src/Components/Contents/ImageSlide.jsx
+++ Frontend/src/Components/Contents/ImageSlide.jsx
@@ -5,19 +5,19 @@ import CarouselDefault from '../../Page/CarouselDefault'
 const ImageSlide = () => {
   return (
     <>
-    <div>
+    <div className='py-5 px-44'>
       <h1 className='text-4xl text-emerald-600'>Unlock Premium Features Now</h1>
     </div>
     <div>
         <CarouselDefault/>
     </div>
     <div>
-        <div className='py-5'>
+        <div className='py-5 px-60'>
             <button type='submit' class="bg-emerald-500 hover:bg-emerald-600 text-white font-bold py-2 px-4 rounded-full w-96">
                 UNLOCK NOW
             </button>
         </div>
-        <div>
+        <div className='px-60'>
             <button type='submit' class="bg-emerald-500 hover:bg-emerald-600 text-white font-bold py-2 px-4 rounded-full w-96">
                 DETAILS
             </button>
--- Frontend/src/Page/CarouselDefault.jsx
+++ Frontend/src/Page/CarouselDefault.jsx
@@ -1,54 +1,56 @@
-import React, { useState } from 'react'
-import {FaArrowRight, FaArrowLeft} from 'react-icons/fa'
-import img1 from '../assets/img1.jpg'
-import img2 from '../assets/img2.jpg'
-import img3 from '../assets/img3.jpg'
+import React, { useState } from 'react';
+import { FaArrowRight, FaArrowLeft } from 'react-icons/fa';
+import img1 from '../assets/img1.jpg';
+import img2 from '../assets/img2.jpg';
+import img3 from '../assets/img3.jpg';
 
 const CarouselDefault = () => {
-    const Image = [
-        {
-            imgURL:img1,
-            text : <p>Lower <br/> Interest Rate</p>
+    const Images = [
+        [
+            { imgURL: img1, text: <p>Lower <br/> Interest Rate</p> },
+            { imgURL: img2, text: <p>Interest <br/> Fee payments</p> },
+            { imgURL: img3, text: <p>Discount on <br/> Materials</p> }
+        ],
+        [
+            { imgURL: img1, text: <p>Lower <br/> Interest Rate</p> },
+            { imgURL: img2, text: <p>Interest <br/> Fee payments</p> },
+            { imgURL: img3, text: <p>Discount on <br/> Materials2</p> }
+        ],
+        [
+            { imgURL: img1, text: <p>Lower <br/> Interest Rate</p> },
+            { imgURL: img2, text: <p>Interest <br/> Fee payments</p> },
+            { imgURL: img3, text: <p>Discount on <br/> Materials3</p> }
+        ],
+    ];
 
-        },
-        {
-            imgURL:img2,
-            text : <p>Interest <br/> Fee payments</p>
-        },
-        {
-            imgURL:img3,
-            text : <p>Discount on <br/> Materials</p>
-        }
-        
-    ]
-    const [ImgIndex,setImgIndex]= useState(0)
-  return (
-    <>
-      <div style={{maxWidth:"1200px",width:"100%",margin:'0 auto'}}>
-        <div className='flex'>
-            {
-                Image.map((item,index)=>{
-                    return (
-                        <>
-                            <div key={index} className='px-4'>
-                                <img src={item.imgURL} alt="" className='img-slider-img' width={300}/>
-                                <p>{item.text}</p>
-                            </div>
-                        </>
-                    )
-                })
-            }
+    const [imgIndex, setImgIndex] = useState(0);
+
+    const nextSlide = () => {
+        setImgIndex((prevIndex) => (prevIndex + 1) % Images.length);
+    };
+
+    const prevSlide = () => {
+        setImgIndex((prevIndex) => (prevIndex === 0 ? Images.length - 1 : prevIndex - 1));
+    };
+
+    return (
+        <div style={{ maxWidth: "1200px", width: "100%", margin: '0 auto', position: "relative" }}>
+            <div className='flex'>
+                {Images[imgIndex].map((item, index) => (
+                    <div key={index} className='px-4'>
+                        <img src={item.imgURL} alt="" className='img-slider-img' width={300}/>
+                        <p>{item.text}</p>
+                    </div>
+                ))}
+            </div>
+            <button onClick={prevSlide} className='img-slider-btn left absolute top-1/2 transform -translate-y-1/2 bg-gray-800 rounded-full p-2 cursor-pointer text-white'>
+                <FaArrowLeft />
+            </button>
+            <button onClick={nextSlide} className='img-slider-btn right-0 absolute top-1/2 transform -translate-y-1/2 bg-gray-800 rounded-full p-2 cursor-pointer text-white' >
+                <FaArrowRight />
+            </button>
         </div>
-        <button>
-        <FaArrowLeft/>
-        </button>
-        <button>
-        <FaArrowRight/>
-            
-        </button>
-      </div>
-    </>
-  )
-}
+    );
+};
 
-export default CarouselDefault
+export default CarouselDefault;
